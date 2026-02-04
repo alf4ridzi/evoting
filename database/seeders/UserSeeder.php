dd355aabae9773f253aed117a3a10faebf89a507
alf4ridzi <aalalfaridzi9@gmail.com>
2026-02-04 15:29:56 +0700

    seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,10 +14,37 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
-        DB::table("users")->insert([
-            "name" => "admin",
-            "email" => "admin@gmail.com",
-            "password" => bcrypt("admin12345")
+        DB::table('users')->insertOrIgnore([
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('admin12345'),
+            ],
+            [
+                'name' => 'user1',
+                'email' => 'user1@gmail.com',
+                'password' => bcrypt('user123'),
+            ],
+            [
+                'name' => 'user2',
+                'email' => 'user2@gmail.com',
+                'password' => bcrypt('user123'),
+            ],
+            [
+                'name' => 'user3',
+                'email' => 'user3@gmail.com',
+                'password' => bcrypt('user123'),
+            ],
+            [
+                'name' => 'user4',
+                'email' => 'user4@gmail.com',
+                'password' => bcrypt('user123'),
+            ],
+            [
+                'name' => 'user5',
+                'email' => 'user5@gmail.com',
+                'password' => bcrypt('user123'),
+            ],
         ]);
     }
 }
